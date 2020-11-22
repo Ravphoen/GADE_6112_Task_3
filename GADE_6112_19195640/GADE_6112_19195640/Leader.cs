0004fe0af6d299084d708c801393944e7fd4116f
Jessica Vardas <56158267+Ravphoen@users.noreply.github.com>
2020-11-22 13:39:38 +0200

    2.4
    Finished leader movement

--- GADE_6112_19195640/GADE_6112_19195640/Leader.cs
+++ GADE_6112_19195640/GADE_6112_19195640/Leader.cs
@@ -10,7 +10,12 @@ namespace GADE_6112_19195640
     {
         public Leader(int _posx, int _posy, string _symbol, int _damage = 2, int _hp = 20) : base(_posx, _posy, _symbol, _damage, _hp)
         {
-
+            base.posx = _posx;
+            base.posy = _posy;
+            base.symbol = _symbol;
+            base.damage = _damage;
+            base.hp = _hp;
+            
         }
 
         private Tile leadertarget;
@@ -20,8 +25,19 @@ namespace GADE_6112_19195640
             get { return leadertarget; }
             set { leadertarget = value; }
         }
-
+        
         public override movement ReturnMove(movement move = 0)
+        {
+            movement movement = ChaseMove(move);
+            if (movement == movement.NoMovement)
+            {
+                RandomMove(move);
+            }
+
+            return move;
+        }
+        //random movement method
+        public movement RandomMove(movement move = 0)
         {
             int randomdir = r.Next(0, 4);
             move = movement.NoMovement;
@@ -83,6 +99,45 @@ namespace GADE_6112_19195640
             }
             return move;
         }
+        //chasing method
+        public movement ChaseMove(movement move = 0)
+        {
+            int xposDifference;
+            int yposDifference;
+            move = movement.NoMovement;           
 
+            xposDifference = base.posx - leadertarget.POSX;
+            yposDifference = base.posy - leadertarget.POSY;
+
+            if (Math.Abs(xposDifference) >= Math.Abs(yposDifference))
+            {
+                if (xposDifference < 0 && CHARACTERVISION[2] is EmptyTile)
+                {
+                    move = movement.Right;
+                    base.posx++;
+                }
+
+                if (xposDifference > 0 && CHARACTERVISION[0] is EmptyTile)
+                {
+                    move = movement.Left;
+                    base.posx--;
+                }
+
+            }
+            if (Math.Abs(yposDifference) > Math.Abs(xposDifference))
+            {
+                if (yposDifference < 0 && CHARACTERVISION[1] is EmptyTile)
+                {
+                    move = movement.Up;
+                    base.posy++;
+                }
+                if (yposDifference > 0 && CHARACTERVISION[3] is EmptyTile)
+                {
+                    move = movement.Down;
+                    base.posy--;
+                }
+            }
+            return move;
+        }   
     }
 }
